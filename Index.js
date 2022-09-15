096fa09f25ef2e638b3416b89cb11de237f6210e
gitpedrohub777 <pedrohb.0222@gmail.com>
2022-09-15 18:03:49 -0300

    JavaScript

---
+++ Index.js
@@ -0,0 +1,30 @@
+/**
+ * Tipos primitivos > String(texto), Number, Boolean
+ * Variáveis JS
+ * Operadores Matemáticos de Js
+ */
+
+// string > texto
+// number > para numeros
+// boolean > boleano
+
+console.log('BORA GIL')
+console.log(5)
+console.log(42.57)
+console.log(true)
+console.log(false)
+
+
+// variáveis > let
+let mensagem = 'cavalo'
+
+console.log(mensagem)
+mensagem = 'CAVALO'
+console.log(mensagem)
+mensagem = 'vacalo'
+console.log(mensagem)
+mensagem = 1
+console.log(mensagem)
+
+
+
